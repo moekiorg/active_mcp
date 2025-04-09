44e34978f4dc59b6d69c25c1e5b2291742f545f6
KAWAKAMI Moeki <hi@moeki.org>
2025-04-09 14:03:38 +0900

    Fix sample prompt

--- demo/http/app/mcp/prompts/hello_prompt.rb
+++ demo/http/app/mcp/prompts/hello_prompt.rb
@@ -1,4 +1,8 @@
 class HelloPrompt < ActiveMcp::Prompt::Base
+  argument :name, required: true, description: "Name", complete: ->(value) do
+    ["UserA", "UserB"].filter { _1.match(value) }
+  end
+
   def initialize(greeting:)
     @greeting = greeting
   end
@@ -11,9 +15,9 @@ class HelloPrompt < ActiveMcp::Prompt::Base
     "This is a test"
   end
 
-  def messages
+  def messages(name:)
     [
-      ActiveMcp::Message::Text.new(role: "user", text: "#{@greeting} #{"a"}"),
+      ActiveMcp::Message::Text.new(role: "user", text: "#{@greeting} #{name}"),
       ActiveMcp::Message::Image.new(role: "user", data:
       File.read(Rails.root.join("public", "lena.png")), mime_type: "image/png"),
       ActiveMcp::Message::Audio.new(role: "user", data: File.read(Rails.root.join("public", "sample.mp3")), mime_type: "audio/mpeg"),
