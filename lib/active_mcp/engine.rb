f1e27fcdb6f6932e7b2e49f4fb84aab2aa0db068
KAWAKAMI Moeki <hi@moeki.org>
2025-04-10 12:48:03 +0900

    Fix autoloading

--- lib/active_mcp/engine.rb
+++ lib/active_mcp/engine.rb
@@ -6,14 +6,11 @@ module ActiveMcp
       [
         Rails.root.join("app", "mcp", "tools"),
         Rails.root.join("app", "mcp", "resources"),
-        Rails.root.join("app", "mcp", "resource_templates"),
         Rails.root.join("app", "mcp", "prompts"),
         Rails.root.join("app", "mcp", "schemas")
-      ].each do |tools_path|
-        if Dir.exist?(tools_path)
-          Dir[tools_path.join("*.rb")].sort.each do |file|
-            require_dependency file
-          end
+      ].each do |path|
+        if Dir.exist?(path)
+          app.autoloaders.main.push_dir(path)
         end
       end
     end
